643005ad8eab46747e1812f06b26e8cf9d53cf23
Sam Collier <samcollier916@gmail.com>
2025-11-12 22:49:25 +0000

    no rpmalloc

--- public/common/TracyAlloc.hpp
+++ public/common/TracyAlloc.hpp
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-#if defined TRACY_ENABLE && !defined __EMSCRIPTEN__
+#if defined TRACY_ENABLE && !defined __EMSCRIPTEN__ && !defined __wii__
 #  include "TracyApi.h"
 #  include "TracyForceInline.hpp"
 #  include "../client/tracy_rpmalloc.hpp"
