6a7b688bc5c7afecde1ab34dafcbf2068d56597e
Sam Collier <samcollier916@gmail.com>
2025-11-12 22:52:07 +0000

    lwp mutex

--- public/common/TracyMutex.hpp
+++ public/common/TracyMutex.hpp
@@ -10,6 +10,22 @@ namespace tracy
 using TracyMutex = std::shared_mutex;
 }
 
+#elif defined __wii__
+#  include <ogc/mutex.h>
+namespace tracy
+{
+class TracyMutex
+{
+    mutex_t m_mutex;
+
+public:
+    TracyMutex() { LWP_MutexInit( &m_mutex, false ); }
+    ~TracyMutex() { LWP_MutexDestroy( m_mutex ); }
+    void lock() { LWP_MutexLock( m_mutex ); }
+    void unlock() { LWP_MutexUnlock( m_mutex ); }
+    bool try_lock() { return LWP_MutexTryLock( m_mutex ) == 0; }
+};
+}
 #else
 
 #include <mutex>
